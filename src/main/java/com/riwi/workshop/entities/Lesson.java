71b3ba9466596b1d98f36040b0bc2230dbd32d4d
alejandroava <alejandroalzate.ava@gmail.com>
2024-08-30 18:38:38 -0500

    add validation lesson and atribuites

--- src/main/java/com/riwi/workshop/entities/Lesson.java
+++ src/main/java/com/riwi/workshop/entities/Lesson.java
@@ -1,10 +1,12 @@
 package com.riwi.workshop.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import com.riwi.workshop.utils.ContentType;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.*;
+
+import java.util.List;
 
 @Entity
 @Getter
@@ -17,5 +19,16 @@ public class Lesson {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotNull(message = "Requiered title")
+    private String title;
+    @NotNull(message = "Requiered description")
+    private String description;
+    private List<ContentType> multimediaContent;
+    @Enumerated(EnumType.STRING)
+    @NotNull(message = "Requiered type Content")
+    private ContentType type;
+    @NotNull(message = "Requiered url")
+    @Size(max = 255,message = "Requiered url")
+    private String url;
 
 }
---
+++ src/main/java/com/riwi/workshop/utils/ContentType.java
@@ -0,0 +1,7 @@
+package com.riwi.workshop.utils;
+
+public enum ContentType {
+    VIDEO,
+    AUDIO,
+    DOCUMENT
+}
