8392ba36d0bc24a998cfd9e18be121a30c54263b
alejandroava <alejandroalzate.ava@gmail.com>
2024-08-30 18:47:07 -0500

    add method save inside service and Autowired lessonRepository

--- src/main/java/com/riwi/workshop/services/impl/LessonModelImpl.java
+++ src/main/java/com/riwi/workshop/services/impl/LessonModelImpl.java
@@ -1,4 +1,15 @@
 package com.riwi.workshop.services.impl;
 
+import com.riwi.workshop.entities.Lesson;
+import com.riwi.workshop.repositories.LessonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class LessonModelImpl {
+
+    @Autowired
+    LessonRepository lessonRepository;
+
+    public Lesson saveLesson(Lesson lesson){
+        return lessonRepository.save(lesson);
+    }
 }
