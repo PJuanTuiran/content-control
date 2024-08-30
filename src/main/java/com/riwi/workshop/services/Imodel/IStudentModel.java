6a4b145bfca72dc795953cd3c78d1b4e2496dcdd
Diego Ramirez <diegomejiasobsu@gmail.com>
2024-08-30 01:59:06 -0500

    update the IStudentModel

--- src/main/java/com/riwi/workshop/services/Imodel/IStudentModel.java
+++ src/main/java/com/riwi/workshop/services/Imodel/IStudentModel.java
@@ -1,10 +1,11 @@
 package com.riwi.workshop.services.Imodel;
 
+import com.riwi.workshop.entities.DTO.StudentOnlyClassInformationDTO;
 import com.riwi.workshop.entities.Student;
 import org.springframework.data.domain.Page;
 
 import java.util.List;
 
 public interface IStudentModel {
-    Page<Student> getByName(String name, int page, int size);
+    Page<StudentOnlyClassInformationDTO> getActiveStudents(int page, int size);
 }
