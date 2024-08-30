b2378f794e09a15961a0e177e499410875bc7e77
Diego Ramirez <diegomejiasobsu@gmail.com>
2024-08-30 00:46:14 -0500

    add the IStudentModel implementation and add the StudentRepository injected dependency

--- src/main/java/com/riwi/workshop/services/impl/StudentModelImpl.java
+++ src/main/java/com/riwi/workshop/services/impl/StudentModelImpl.java
@@ -1,7 +1,20 @@
 package com.riwi.workshop.services.impl;
 
+import com.riwi.workshop.entities.Student;
+import com.riwi.workshop.repositories.StudentRepository;
+import com.riwi.workshop.services.Imodel.IStudentModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 
 @Service
-public class StudentModelImpl {
+public class StudentModelImpl implements IStudentModel {
+
+    @Autowired
+    StudentRepository studentRepository;
+
+    @Override
+    public Page<Student> getByName(String name, int page, int size) {
+        return null;
+    }
 }
