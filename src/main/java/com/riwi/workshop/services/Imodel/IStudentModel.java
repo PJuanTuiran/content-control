aebcdd8d2b1664c270c59feccbc61d7cf8e37803
Diego Ramirez <diegomejiasobsu@gmail.com>
2024-08-30 00:44:16 -0500

    add the method getByName with the parameters in iStudentModel

--- src/main/java/com/riwi/workshop/services/Imodel/IStudentModel.java
+++ src/main/java/com/riwi/workshop/services/Imodel/IStudentModel.java
@@ -1,4 +1,10 @@
 package com.riwi.workshop.services.Imodel;
 
+import com.riwi.workshop.entities.Student;
+import org.springframework.data.domain.Page;
+
+import java.util.List;
+
 public interface IStudentModel {
+    Page<Student> getByName(String name, int page, int size);
 }
