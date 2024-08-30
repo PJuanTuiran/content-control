9f0f46865e5a488882f616093e4eef7b2c445ca3
alejandroava <alejandroalzate.ava@gmail.com>
2024-08-30 18:43:39 -0500

    add jparepository to lesson

--- src/main/java/com/riwi/workshop/repositories/LessonRepository.java
+++ src/main/java/com/riwi/workshop/repositories/LessonRepository.java
@@ -1,4 +1,7 @@
 package com.riwi.workshop.repositories;
 
-public interface LessonRepository {
+import com.riwi.workshop.entities.Lesson;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LessonRepository extends JpaRepository<Lesson, Long> {
 }
