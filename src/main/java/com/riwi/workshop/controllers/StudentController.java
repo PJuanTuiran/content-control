3d8b0b565c02e62c3327ce32fdb25fc6cb518f8a
Diego Ramirez <diegomejiasobsu@gmail.com>
2024-08-30 12:25:11 -0500

    add the implementation to getById in the student controller

--- src/main/java/com/riwi/workshop/controllers/StudentController.java
+++ src/main/java/com/riwi/workshop/controllers/StudentController.java
@@ -1,13 +1,13 @@
 package com.riwi.workshop.controllers;
 
 import com.riwi.workshop.entities.DTO.StudentOnlyClassInformationDTO;
+import com.riwi.workshop.entities.Student;
 import com.riwi.workshop.services.impl.StudentModelImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/")
@@ -23,6 +23,13 @@ public class StudentController {
             @RequestParam(value = "name", required = false) String name
     ){
         return studentModel.getActiveStudents(page, size, name);
+    }
+
+    @GetMapping("students/{id}")
+    public Optional<Student> getById(
+            @PathVariable Long id
+    ){
+        return studentModel.getById(id);
     }
 
 
