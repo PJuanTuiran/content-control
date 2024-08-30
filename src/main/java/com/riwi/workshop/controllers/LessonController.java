be594285bbf4fc8be2a977665ccf70006e4bee50
alejandroava <alejandroalzate.ava@gmail.com>
2024-08-30 18:58:57 -0500

    add method saveLesson inside controller

--- src/main/java/com/riwi/workshop/controllers/LessonController.java
+++ src/main/java/com/riwi/workshop/controllers/LessonController.java
@@ -1,4 +1,25 @@
 package com.riwi.workshop.controllers;
 
+import com.riwi.workshop.entities.Lesson;
+import com.riwi.workshop.services.impl.LessonModelImpl;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@RequestMapping("/api/vi/lesson")
 public class LessonController {
+
+    @Autowired
+    LessonModelImpl lessonModel;
+
+    @PostMapping
+    public ResponseEntity<Lesson> saveLesson(@RequestBody @Valid Lesson lesson){
+        return ResponseEntity.status(HttpStatus.CREATED).body(lessonModel.saveLesson(lesson));
+    }
+
 }
