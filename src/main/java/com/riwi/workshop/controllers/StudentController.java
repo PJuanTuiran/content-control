e18612f95e99fbbe5461e5b1e95fce13c0bbda4f
Diego Ramirez <diegomejiasobsu@gmail.com>
2024-08-30 12:02:59 -0500

    add the method getActiveStudents in the student controller

--- src/main/java/com/riwi/workshop/controllers/StudentController.java
+++ src/main/java/com/riwi/workshop/controllers/StudentController.java
@@ -1,7 +1,29 @@
 package com.riwi.workshop.controllers;
 
+import com.riwi.workshop.entities.DTO.StudentOnlyClassInformationDTO;
+import com.riwi.workshop.services.impl.StudentModelImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/v1/")
 public class StudentController {
+
+    @Autowired
+    StudentModelImpl studentModel;
+
+    @GetMapping("students")
+    public Page<StudentOnlyClassInformationDTO> getActiveStudents(
+            @RequestParam(value = "page", defaultValue = "0") int page,
+            @RequestParam(value = "size", defaultValue = "10") int size,
+            @RequestParam(value = "name", required = false) String name
+    ){
+        return studentModel.getActiveStudents(page, size, name);
+    }
+
+
 }
