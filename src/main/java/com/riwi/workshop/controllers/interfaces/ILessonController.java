36ac300adaa3ce8f36d6240af0807899a9a21551
alejandroava <alejandroalzate.ava@gmail.com>
2024-08-31 01:26:06 -0500

    add generic create and interfaces imodellessonController

---
+++ src/main/java/com/riwi/workshop/controllers/interfaces/ILessonController.java
@@ -0,0 +1,8 @@
+package com.riwi.workshop.controllers.interfaces;
+
+import com.riwi.workshop.controllers.generic.Create;
+import com.riwi.workshop.entities.Lesson;
+
+public interface ILessonController extends
+        Create<Lesson> {
+}
