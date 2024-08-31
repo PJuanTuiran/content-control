92484e7a39120fd75c99a54bff533f534acaa8c2
Diego Ramirez <diegomejiasobsu@gmail.com>
2024-08-30 23:59:25 -0500

    add the definition to createModel and extends it in IstudentModel

--- src/main/java/com/riwi/workshop/services/Imodel/IStudentModel.java
+++ src/main/java/com/riwi/workshop/services/Imodel/IStudentModel.java
@@ -1,12 +1,14 @@
 package com.riwi.workshop.services.Imodel;
 
+import com.riwi.workshop.entities.DTO.StudentCreateDTO;
 import com.riwi.workshop.entities.DTO.StudentOnlyClassInformationDTO;
 import com.riwi.workshop.entities.Student;
+import com.riwi.workshop.services.crud.CreateModel;
 import com.riwi.workshop.services.crud.GetByIdModel;
 import org.springframework.data.domain.Page;
 
 import java.util.List;
 
-public interface IStudentModel extends GetByIdModel<Student, Long> {
+public interface IStudentModel extends GetByIdModel<Student, Long>, CreateModel<Student, StudentCreateDTO> {
     Page<StudentOnlyClassInformationDTO> getActiveStudents(int page, int size, String name);
 }
