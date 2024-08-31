d4d4fbfabb446c3d22f77ad690d8cb7c67866e60
alejandroava <alejandroalzate.ava@gmail.com>
2024-08-31 18:05:36 -0500

    add method getById in IlessonController

--- src/main/java/com/riwi/workshop/controllers/interfaces/ILessonController.java
+++ src/main/java/com/riwi/workshop/controllers/interfaces/ILessonController.java
@@ -1,10 +1,12 @@
 package com.riwi.workshop.controllers.interfaces;
 
 import com.riwi.workshop.controllers.generic.Create;
+import com.riwi.workshop.controllers.generic.GetById;
 import com.riwi.workshop.entities.Lesson;
 import org.springframework.http.ResponseEntity;
 
 public interface ILessonController extends
-        Create<Lesson> {
+        Create<Lesson>,
+        GetById<Lesson,Long> {
     ResponseEntity<Lesson> disableLesosnById(Long id);
 }
