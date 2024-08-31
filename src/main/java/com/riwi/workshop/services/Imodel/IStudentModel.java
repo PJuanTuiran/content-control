3c4def5b62ea90f905f36cf90213f566d93ac8a2
Diego Ramirez <diegomejiasobsu@gmail.com>
2024-08-31 18:00:10 -0500

    add the instance of disableStudent method in student model interface

--- src/main/java/com/riwi/workshop/services/Imodel/IStudentModel.java
+++ src/main/java/com/riwi/workshop/services/Imodel/IStudentModel.java
@@ -2,13 +2,16 @@ package com.riwi.workshop.services.Imodel;
 
 import com.riwi.workshop.entities.DTO.StudentCreateDTO;
 import com.riwi.workshop.entities.DTO.StudentOnlyClassInformationDTO;
+import com.riwi.workshop.entities.DTO.StudentResponseDTO;
 import com.riwi.workshop.entities.Student;
 import com.riwi.workshop.services.crud.CreateModel;
 import com.riwi.workshop.services.crud.GetByIdModel;
 import org.springframework.data.domain.Page;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface IStudentModel extends GetByIdModel<Student, Long>, CreateModel<Student, StudentCreateDTO> {
     Page<StudentOnlyClassInformationDTO> getActiveStudents(int page, int size, String name);
+    Optional<StudentResponseDTO> disableStudent(Long id);
 }
